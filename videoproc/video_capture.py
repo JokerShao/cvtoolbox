426b9b6f1c25061573d3b6838deceaf634b3609b
zexishao <zexishao@foxmail.com>
2019-11-14 14:55:54 +0800

    add some dl tutorial

--- videoproc/video_capture.py
+++ videoproc/video_capture.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture(0)
+camid = 64
+
+cap = cv2.VideoCapture('rtsp://admin:admin888@192.168.1.'+str(camid)+':554//Streaming/Channels/1')
+cnt = 0
 
 while True:
     retval, frame = cap.read()
@@ -10,6 +13,9 @@ while True:
         retkey = cv2.waitKey(1) & 0xFF
         if retkey == ord('q'):
             break
+        elif retkey == ord('s'):
+            cv2.imwrite(str(cnt)+'.jpg', frame)
+            cnt+=1
 
 cv2.destroyAllWindows()
 
