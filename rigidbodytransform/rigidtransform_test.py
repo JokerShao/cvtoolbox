f6616ad3f940b0e5b1a2f6f84056e0004e932e75
zexishao <zexishao@foxmail.com>
2020-09-13 18:37:39 +0800

    rigidtransform: cleanup code, optimize efficient

---
+++ rigidbodytransform/rigidtransform_test.py
@@ -0,0 +1,78 @@
+import time
+import numpy as np
+from rigidtransform import \
+    rotm2quat, quat2rotm, axang2quat, \
+    quat2axang, axang2rotm, rotm2axang
+from quatmath import quatrotate
+
+
+if __name__ == '__main__':
+
+    A = np.random.random((3,3))
+    R, _ = np.linalg.qr(A)
+    p = np.random.random((3,1))
+
+    print('R:\n', R, '\n')
+
+    Rtoquat = rotm2quat(R)
+    print('Rtoquat:\n', Rtoquat, '\n')
+
+    quattoaxang = quat2axang(Rtoquat)
+    print('quattoaxang:\n', quattoaxang, '\n')
+
+    axangtorotm = axang2rotm(quattoaxang)
+    print('axangtorotm:\n', axangtorotm, '\n')
+
+    rotmtoaxang = rotm2axang(axangtorotm)
+    print('rotmtoaxang:\n', rotmtoaxang, '\n')
+
+    axangtoquat = axang2quat(rotmtoaxang)
+    print('axangtoquat:\n', axangtoquat, '\n')
+
+    quattorotm = quat2rotm(axangtoquat)
+    print('quattorotm:\n', quattorotm, '\n')
+
+    p1 = R@p
+    print('p1\n', p1, '\n')
+
+    p2 = quatrotate(axangtoquat, p)
+    print('p2\n', p2, '\n')
+
+    time.sleep(1)
+
+    time_begin = time.time()
+    for i in range(500000):
+        axang = rotm2quat(R)
+    time_end = time.time()
+    print('average:', (time_end-time_begin)*1000/500000, 'ms')
+
+    time_begin = time.time()
+    for i in range(500000):
+        axang = quat2axang(Rtoquat)
+    time_end = time.time()
+    print('average:', (time_end-time_begin)*1000/500000, 'ms')
+
+    time_begin = time.time()
+    for i in range(500000):
+        R = axang2rotm(quattoaxang)
+    time_end = time.time()
+    print('average:', (time_end-time_begin)*1000/500000, 'ms')
+
+    time_begin = time.time()
+    for i in range(500000):
+        axang = rotm2axang(axangtorotm)
+    time_end = time.time()
+    print('average:', (time_end-time_begin)*1000/500000, 'ms')
+
+    time_begin = time.time()
+    for i in range(500000):
+        R = axang2quat(rotmtoaxang)
+    time_end = time.time()
+    print('average:', (time_end-time_begin)*1000/500000, 'ms')
+
+    time_begin = time.time()
+    for i in range(500000):
+        R = quat2rotm(axangtoquat)
+    time_end = time.time()
+    print('average:', (time_end-time_begin)*1000/500000, 'ms')
+
