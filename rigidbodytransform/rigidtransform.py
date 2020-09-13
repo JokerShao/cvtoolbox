2bb95cdfa557a03359189a56517b81e12612ee94
zexishao <zexishao@foxmail.com>
2020-09-13 20:30:04 +0800

    1. remove useless code 2. add rvec operation

--- rigidbodytransform/rigidtransform.py
+++ rigidbodytransform/rigidtransform.py
@@ -2,9 +2,12 @@
 Rigid body basic transform, convert between axis angle,
 rotation matrix and quaternion.
 dtype: ndarray
-Q shape:     1x4  w x y z     order
-AXANG shape: 1x4  x y z theta order
+
+q shape:     1x4  w x y z     order
+axang shape: 1x4  x y z theta order
+rvec shape:  1x3  x y z       order
 R is right multiply matrix, for example:  p1 = R * p
+
 author: zexi shao
 email: zexishao@foxmail.com
 """
@@ -90,6 +93,69 @@ def rotm2axang(R):
     axang = empty((1,4))
     axang[0,0], axang[0,1], axang[0,2], axang[0,3] = x/norm, y/norm, z/norm, theta
     return axang
+
+def rvec2axang(rvec):
+    x, y, z = rvec[0,0], rvec[0,1], rvec[0,2]
+    theta = sqrt(x*x+y*y+z*z)
+    axang = empty((1,4))
+    axang[0,0], axang[0,1], axang[0,2], axang[0,3] = \
+        x/theta, y/theta, z/theta, theta
+    return axang
+
+def axang2rvec(axang):
+    x, y, z, theta = axang[0,0], axang[0,1], axang[0,2], axang[0,3]
+    rvec = empty((1,3))
+    rvec[0,0], rvec[0,1], rvec[0,2] = x*theta, y*theta, z*theta
+    return rvec
+
+def rvec2rotm(rvec):
+    x, y, z = rvec[0,0], rvec[0,1], rvec[0,2]
+    theta = sqrt(x*x+y*y+z*z)
+    x, y, z = x/theta, y/theta, z/theta
+    ctheta, stheta = cos(theta), sin(theta)
+    ctheta_d1 = 1-ctheta
+
+    R = empty((3,3))
+    R[0,0], R[0,1], R[0,2], R[1,0], R[1,1], R[1,2], R[2,0], R[2,1], R[2,2] = \
+        ctheta+ctheta_d1*x*x, ctheta_d1*x*y-stheta*z, ctheta_d1*x*z+stheta*y, \
+        ctheta_d1*y*x+stheta*z, ctheta+ctheta_d1*y*y, ctheta_d1*y*z-stheta*x, \
+        ctheta_d1*z*x-stheta*y, ctheta_d1*z*y+stheta*x, ctheta+ctheta_d1*z*z
+    return R
+
+def rotm2rvec(R):
+    r11, r12, r13, r21, r22, r23, r31, r32, r33 = \
+        R[0,0], R[0,1], R[0,2], \
+        R[1,0], R[1,1], R[1,2], \
+        R[2,0], R[2,1], R[2,2]
+    theta = acos((r11+r22+r33-1)/2)
+    tmp = 1/(2*sin(theta))
+    x, y, z = tmp*(r32-r23), tmp*(r13-r31), tmp*(r21-r12)
+    rvec = empty((1,3))
+    rvec[0,0], rvec[0,1], rvec[0,2] = x*theta, y*theta, z*theta
+    return rvec
+
+def quat2rvec(q):
+    q0, q1, q2, q3 = q[0,0], q[0,1], q[0,2], q[0,3]
+    norm = q0*q0+q1*q1+q2*q2+q3*q3
+    q0, q1, q2, q3 = q0/norm, q1/norm, q2/norm, q3/norm
+    theta = acos(q0)*2
+
+    rvec = empty((1,3))
+    if theta < 1e-10:
+        rvec[0,0], rvec[0,1], rvec[0,2] = 1,0,0
+    else:
+        sth = sin(theta/2)
+        x, y, z = q1/sth, q2/sth, q3/sth
+        rvec[0,0], rvec[0,1], rvec[0,2]= x*theta, y*theta, z*theta
+    return rvec
+
+def rvec2quat(rvec):
+    x, y, z = rvec[0,0], rvec[0,1], rvec[0,2]
+    theta = sqrt(x*x+y*y+z*z)
+    sht = sin(theta/2)
+    q = empty((1,4))
+    q[0,0], q[0,1], q[0,2], q[0,3] = cos(theta/2), x/theta*sht, y/theta*sht, z/theta*sht
+    return q
 
 def skew(v):
     """ input 3x1 vector """
