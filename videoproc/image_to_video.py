8f2b890b06e4098488fa62a4a9a8c78de0b06176
zexishao <zexishao@foxmail.com>
2020-12-16 16:23:06 +0800

    optimize image_to_video.py and video_capture.py

--- videoproc/image_to_video.py
+++ videoproc/image_to_video.py
@@ -15,73 +15,66 @@ flv file support list:
 import numpy as np
 import cv2
 
-def video2imageseq(videopath, imgseqpath):
+
+def imageseq2video( \
+    img_prefix_path='./', vid_output_name='out.mp4', \
+    img_type_name='png', start_idx=0, duration_num=75, \
+    fps=24.0, vid_w=1920, vid_h=1080):
     """
-    convert a video to a image sequence.
+    img_prefix_path 图像路径前缀
+    vid_output_name 视频输出文件名
+    start_idx 图像起始序号
+    duration_num 图像持续帧数
+    fps 输出视频帧率
+    vid_w 输出视频宽度
+    vid_h 输出视频高度
     """
-    cnt = 0
-
-    capture = cv2.VideoCapture(videopath)
-
-    while True:
-        retval, frame = capture.read()
-        if not retval:
-            break
-        if cnt % 5 == 0: # 取一半视频帧
-            cv2.imwrite(imgseqpath+'/'+str(cnt)+'.jpg', frame)
-        cnt += 1
-    capture.release()
-
-# def imageseq2video(imgseqpath, videopath):
-def imageseq2video():
-    """ doc string """
-    cnt = 165 # 图像开始序号
-    number = 727 # 图像持续帧数
-
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    vw = cv2.VideoWriter('./out.mp4', fourcc, 24.0, (1920, 1080))
+    vw = cv2.VideoWriter(vid_name, fourcc, fps, (vid_w, vid_h))
 
-    for nn in range(number):
-        imgpath = 'D:\\DataRepository\\greenscreen\\test2\\xushi\\Image2\\Map_YanShi.'+str(cnt+nn)+'.jpg'
+    for nn in range(duration_num):
+        imgpath = img_prefix_path+str(start_idx+nn)+'.'+img_type_name
         print(nn)
         img = cv2.imread(imgpath)
         vw.write(img)
-    
+
     vw.release()
 
-# def imageseq2video(imgseqpath, videopath):
-def imageseq2video0():
-    """ doc string """
-    cnt = 0 # 图像开始序号
-    number = 1900 # 图像持续帧数
-
-    target = np.zeros((1080, 960, 3), dtype=np.uint8)
+def imageseq_merge2video( \
+    img0_prefix_path='./img0/', img1_prefix_path='./img1/', \
+    img_type_name='png', vid_output_name='out.mp4', \
+    img0_start_idx=0, img1_start_idx=0, duration_num=75, \
+    fps=24.0, vid_w=960, vid_h=1080):
+    """
+    合并对齐两幅图像，并压入视频文件
+    img0_start_idx 源图像0 开始序号
+    duration_num 持续帧数
+    """
+    # 合并后的缓存图像
+    target = np.zeros((vid_h, vid_w, 3), dtype=np.uint8)
 
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    vw = cv2.VideoWriter('./outsplit.avi', fourcc, 24.0, (960, 1080))
+    vw = cv2.VideoWriter(vid_output_name, fourcc, fps, (vid_w, vid_h))
 
-    for nn in range(number):
-        srcpath = 'D:\\greenscreen\\recording5_huawei_output\\'+str(nn+cnt)+'.png'
-        dstpath = 'D:\\greenscreen\\recording5_huawei\\'+str(nn+cnt)+'.png'
-        src = cv2.imread(srcpath)
-        dst = cv2.imread(dstpath)
-        src_small = cv2.resize(src, (960, 540))
-        dst_small = cv2.resize(dst, (960, 540))
+    for nn in range(duration_num):
+        srcpath0 = img0_prefix_path+str(nn+img0_start_idx)+'.'+img_type_name
+        srcpath1 = img1_prefix_path+str(nn+img1_start_idx)+'.'+img_type_name
+        src0 = cv2.imread(srcpath0)
+        src1 = cv2.imread(srcpath1)
+        src0_small = cv2.resize(src0, (vid_w, vid_h//2))
+        src1_small = cv2.resize(src1, (vid_w, vid_h//2))
 
-        target[0:540, :, :] = src_small
-        target[540:1080, :, :] = dst_small
-
+        target[0:vid_h//2, :, :] = src0_small
+        target[vid_h//2:vid_h, :, :] = src1_small
         cv2.imshow('target', target)
         cv2.waitKey(1)
-
         print(nn+cnt)
-        # img = cv2.imread(imgpath)
         vw.write(target)
     vw.release()
 
-if __name__ == '__main__':
-    # videopath = 'E:\\P4Workspace\\calibration-package\\external_params\\00015.MTS'
-    # imgseqpath = 'E:\\P4Workspace\\calibration-package\\external_params\\00015\\'
 
-    # video2imageseq(videopath, imgseqpath)
-    imageseq2video0()
+if __name__ == '__main__':
+    # videopath = 'video_aruco.mp4'
+    # imgseqpath = 'imgs/'
+    imageseq2video()
+
--- videoproc/video_capture.py
+++ videoproc/video_capture.py
@@ -1,31 +1,32 @@
 import cv2
 
+
+# 参数配置
+cam_w = 1920
+cam_h = 1080
+cam_fps = 30
+
 # 相机初始化
 cap = cv2.VideoCapture(0)
-ret = cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280*2)
-ret = cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
-ret = cap.set(cv2.CAP_PROP_FPS, 60)
-fourcc = cv2.VideoWriter_fourcc(*'YUY2')
+ret = cap.set(cv2.CAP_PROP_FRAME_WIDTH, cam_w)
+ret = cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_h)
+ret = cap.set(cv2.CAP_PROP_FPS, cam_fps)
+# fourcc = cv2.VideoWriter_fourcc(*'YUY2')
+fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 ret = cap.set(cv2.CAP_PROP_FOURCC, fourcc)
 
 cnt = 0
-cntt = 0
-
 while True:
     retval, frame = cap.read()
 
-    small_frame = cv2.resize(frame, (1280, 360))
-
     if retval is True:
-        cv2.imshow('frame', small_frame)
+        cv2.imshow('frame', frame)
         retkey = cv2.waitKey(1) & 0xFF
         if retkey == ord('q'):
             break
-        # elif retkey == ord('g'):
-        if cnt >= 3:
-            cv2.imwrite('./hand/'+str(cntt)+'.png', frame)
+        elif retkey == ord('g'):
+            cv2.imwrite('./imgs/'+str(cnt)+'.png', frame)
             cnt = 0
-            cntt += 1
         cnt+=1
 
 cv2.destroyAllWindows()
