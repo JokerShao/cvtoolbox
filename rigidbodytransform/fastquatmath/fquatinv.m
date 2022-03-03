5067f4f85cc0174223e999d59b9cb9316de815ad
zexi <zexishao@foxmail.com>
2022-03-03 11:18:18 +0800

    style(fastquatmath): compact some quat unpack line

--- rigidbodytransform/fastquatmath/fquatinv.m
+++ rigidbodytransform/fastquatmath/fquatinv.m
@@ -1,9 +1,6 @@
 function qinv = fquatinv(q)
     % 1x4 w x y z order
-    q0 = q(1,1);
-    q1 = q(1,2);
-    q2 = q(1,3);
-    q3 = q(1,4);
+    q0=q(1,1); q1=q(1,2); q2=q(1,3); q3=q(1,4);
     norm2 = (q0*q0+q1*q1+q2*q2+q3*q3);
     qinv = [q0/norm2 -q1/norm2 -q2/norm2 -q3/norm2];
 end
--- rigidbodytransform/fastquatmath/fquatnormalize.m
+++ rigidbodytransform/fastquatmath/fquatnormalize.m
@@ -1,9 +1,6 @@
 function qnorm = fquatnormalize(q)
     % 1x4 w x y z order
-    q0 = q(1,1);
-    q1 = q(1,2);
-    q2 = q(1,3);
-    q3 = q(1,4);
+    q0=q(1,1); q1=q(1,2); q2=q(1,3); q3=q(1,4);
     norm = sqrt(q0*q0+q1*q1+q2*q2+q3*q3);
     qnorm = q/norm;
 end
